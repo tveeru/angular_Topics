3e34327de1f5d9fef9baddfa01ef87f44a2986cd
veer <anjaneyulu.th@gmail.com>
2022-04-30 17:02:01 +0530

    dummy template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ChildComponent } from './child/child.component';
-
 @NgModule({
   declarations: [
-    AppComponent,
-      ChildComponent
+    AppComponent
    ],
   imports: [
     BrowserModule,
