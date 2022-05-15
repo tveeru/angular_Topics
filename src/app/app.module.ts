fcaa1401b72583279eaeaf529dec98fda584c348
veer <anjaneyulu.th@gmail.com>
2022-05-15 20:05:48 +0530

    removed app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ChildComponent } from './child/child.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-      ChildComponent
-   ],
+    AppComponent
+     ],
   imports: [
     BrowserModule,
     AppRoutingModule,
