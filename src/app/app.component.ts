64997b47f5cc34326d6839aba3bebfb91963c851
veer <anjaneyulu.th@gmail.com>
2022-06-01 15:08:56 +0530

    data added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  myObservable = new Observable((obs) => {
+    console.log("Observable starts working...");
+
+  setTimeout(() => {
+    obs.next("1");
+  }, 1000);
+  setTimeout(() => {
+    obs.next("2");
+  }, 2000);
+  setTimeout(() => {
+    obs.next("3");
+
+  }, 3000);
+  })
+
+  ngOnInit(): void {
+    this.myObservable.subscribe(x => console.log(x));
+  }
 
 }
