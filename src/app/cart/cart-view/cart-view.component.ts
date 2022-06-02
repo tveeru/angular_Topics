8627710d579897957d51ae2c4c614339f6185a39
veer <anjaneyulu.th@gmail.com>
2022-06-02 12:28:39 +0530

    second method

---
+++ src/app/cart/cart-view/cart-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-view',
+  templateUrl: './cart-view.component.html',
+  styleUrls: ['./cart-view.component.css']
+})
+export class CartViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
